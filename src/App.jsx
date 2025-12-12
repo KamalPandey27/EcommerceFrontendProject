b52d660711d3b2b4adf67203bda20a39b8c07202
kamalpandey27 <kamal0000027@gmail.com>
2025-12-12 22:27:58 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="products" element={<Products />} />
-        <Route path="Cart" element={<Cart />} />
+        <Route path="cart" element={<Cart />} />
       </Route>
     )
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { FiMenu, FiX } from "react-icons/fi";
 import { NavLink } from "react-router-dom";
 function Navbar() {
+  const [menu, setMenu] = useState("hidden");
+  const closeMenu = () => {
+    setMenu("hidden");
+  };
+  const openMenu = () => {
+    setMenu("flex");
+  };
   return (
     <>
-      <header className="shadow-lg sticky top-0 italic">
-        <nav className="flex justify-around  p-3">
+      <header className="shadow-lg sticky top-0 italic z-10">
+        <nav className="flex md:justify-around md:p-3 p-4 justify-between">
           <div className="flex gap-7 text-lg items-center justify-center">
             <div className="font-bold text-3xl first-letter:text-red-500">
               Zaptro
@@ -13,8 +21,10 @@ function Navbar() {
             <div>Add Address</div>
           </div>
 
-          <div className="flex items-center justify-center">
-            <ul className="flex gap-5 ">
+          <div
+            className={` md:flex ${menu} md:items-center md:justify-center md:static md:h-auto md:w-auto absolute right-0 top-0 h-screen bg-white w-[50vw]`}
+          >
+            <ul className=" md:flex md:flex-row md:gap-5 flex flex-col gap-7 md:p-0 p-10 w-full ">
               <li>
                 <NavLink
                   to="/"
@@ -75,17 +85,27 @@ function Navbar() {
                   Contact
                 </NavLink>
               </li>
-              <li>
+              <li className="md:inline-block  hidden">
                 <NavLink to="/Cart" className="cursor-pointer">
                   <FaShoppingCart size={30} />
                 </NavLink>
               </li>
               <li>
-                <button className="bg-red-500 text-white w-[73px] h-[32px] rounded cursor-pointer">
+                <button className="bg-red-500 text-white md:w-[73px] md:h-[32px] rounded cursor-pointer w-full h-9 text-[18px] ">
                   Sign in
                 </button>
               </li>
+              <li className="absolute bottom-18 text-[18px] md:hidden inline-block">
+                Made with ❤️ by Kamal
+              </li>
             </ul>
+          </div>
+          <div className={`${menu} z-10 md:hidden `} onClick={closeMenu}>
+            <FiX size={30} className="cursor-pointer absolute right-5" />
+          </div>
+
+          <div className={`md:hidden`} onClick={openMenu}>
+            <FiMenu size={28} className="cursor-pointer" />
           </div>
         </nav>
       </header>
